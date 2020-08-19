fd6b9017c0361510fc0c526d28e1a6d2511467da
jvanderweerd <jvanderweerd@appannie.com>
2020-08-19 20:12:31 +0200

    initial commit

---
+++ src/TelescopeAppServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+namespace JillevdW\TelescopeApp;
+use Illuminate\Support\ServiceProvider;
+
+class TelescopeAppServiceProvider extends ServiceProvider
+{
+    /**
+    * Publishes configuration file.
+    *
+    * @return  void
+    */
+    public function boot()
+    {
+        
+    }
+
+    /**
+    * Make config publishment optional by merging the config from the package.
+    *
+    * @return  void
+    */
+    public function register()
+    {
+
+    }
+}
